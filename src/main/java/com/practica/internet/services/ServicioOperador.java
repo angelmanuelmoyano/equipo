1bd53c9265602d305e532c9b8a5cf3d9359684fd
angel <angelm.moyano@gmail.com>
2021-11-06 20:09:46 -0300

    actualizacion servicio operador y servicio producto

--- src/main/java/com/practica/internet/services/ServicioOperador.java
+++ src/main/java/com/practica/internet/services/ServicioOperador.java
@@ -1,4 +1,77 @@
 package com.practica.internet.services;
 
-public class ServicioOperador {
+import com.practica.internet.entities.Operador;
+import com.practica.internet.repositories.RepositorioOperador;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+public class ServicioOperador  implements  ServicioBase<Operador>{
+    @Autowired
+    private RepositorioOperador repositorio;
+    @Override
+    @Transactional
+    public List<Operador> findAll() throws Exception {
+        try {
+            List<Operador> operador = this.repositorio.findAll();
+            return operador;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Operador findById(long id) throws Exception {
+        try {
+            Optional<Operador> opt = this.repositorio.findById(id);
+            return opt.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Operador saveOne(Operador entity) throws Exception {
+        try {
+            Operador operador = this.repositorio.save(entity);
+            return operador;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Operador updateOne(Operador entity, long id) throws Exception {
+        try {
+            Optional<Operador> opt = this.repositorio.findById(id);
+            Operador operador = opt.get();
+            operador = this.repositorio.save(entity);
+            return operador;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteById(long id) throws Exception {
+        try {
+            Optional<Operador> opt = this.repositorio.findById(id);
+            if (!opt.isEmpty()) {
+                Operador operador = opt.get();
+                // actividad_producto.setActivo(!actividad_producto.isActivo());
+                this.repositorio.save(operador);
+            } else {
+                throw new Exception();
+            }
+            return true;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
